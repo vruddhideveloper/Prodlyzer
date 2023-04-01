8201ea01ab42ed813716edecbaccedb4bdcbe4e2
Vruddhi <toliavruddhi@gmail.com>
2023-04-01 11:15:26 +0530

    final commit

--- AdminDashboard/backend/models/productClickModel.js
+++ AdminDashboard/backend/models/productClickModel.js
@@ -24,6 +24,7 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// models exported
 const User = new mongoose.model("User", userSchema);
 const Product = new mongoose.model("Product", productSchema);
 
